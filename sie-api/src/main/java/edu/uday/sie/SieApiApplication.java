0a7a00a5a0e6dfb9daa421ac68662ee4d571ec23
AndreiTorres <andreitorres09@outlook.com>
2023-06-28 11:35:12 -0600

    Modificando nombre de la aplicación

--- sie-api/src/main/java/edu/uday/sie/SieApiApplication.java
+++ sie-api/src/main/java/edu/uday/sie/SieApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoaApiApplication {
+public class SieApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoaApiApplication.class, args);
+		SpringApplication.run(SieApiApplication.class, args);
 	}
 
 }
